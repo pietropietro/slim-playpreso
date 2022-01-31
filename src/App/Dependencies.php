38be4f132f630e365bb63ed32a6436dd4489c5db
pietro <mini.pietro@associazionerousseau.it>
2022-02-01 00:03:44 +0100

    start porting

--- src/App/Dependencies.php
+++ src/App/Dependencies.php
@@ -6,21 +6,15 @@ use App\Handler\ApiError;
 use App\Service\RedisService;
 use Psr\Container\ContainerInterface;
 
-$container['db'] = static function (ContainerInterface $container): PDO {
-    $database = $container->get('settings')['db'];
-    $dsn = sprintf(
-        'mysql:host=%s;dbname=%s;port=%s;charset=utf8',
-        $database['host'],
-        $database['name'],
-        $database['port']
-    );
-    $pdo = new PDO($dsn, $database['user'], $database['pass']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-    return $pdo;
-};
+require_once __DIR__ . '/../vendor/mysqli.php';
+$container['db'] = new MysqliDb(
+    $database['host'],
+    $database['user'],
+    $database['pass'],
+    $database['name'],
+    $database['port']
+);
+$database = $container->get('settings')['db'];
 
 $container['errorHandler'] = static fn (): ApiError => new ApiError();
 
--- src/App/Repositories.php
+++ src/App/Repositories.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-use App\Repository\NoteRepository;
-use App\Repository\TaskRepository;
+use App\Repository\MatchRepository;
+use App\Repository\GuessRepository;
 use App\Repository\UserRepository;
 use Psr\Container\ContainerInterface;
 
 $container['user_repository'] = static fn (ContainerInterface $container): UserRepository => new UserRepository($container->get('db'));
 
-$container['task_repository'] = static fn (ContainerInterface $container): TaskRepository => new TaskRepository($container->get('db'));
+$container['task_repository'] = static fn (ContainerInterface $container): GuessRepository => new GuessRepository($container->get('db'));
 
-$container['note_repository'] = static fn (ContainerInterface $container): NoteRepository => new NoteRepository($container->get('db'));
+$container['note_repository'] = static fn (ContainerInterface $container): MatchRepository => new MatchRepository($container->get('db'));
