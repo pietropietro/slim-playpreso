5def1cd0dea75ebadd643ed1f44f65cb220bae93
pietro <mini.pietro@associazionerousseau.it>
2022-03-21 17:44:57 +0100

    fix routes

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -8,34 +8,31 @@ use App\Controller\User;
 use App\Middleware\Auth;
 
 return function ($app) {
-    $app->group('/v1TT', function () use ($app): void {
+    $app->add(function ($req, $res, $next) {
+        $response = $next($req, $res);
+        return $response
+                ->withHeader('Access-Control-Allow-Origin', $_SERVER['ALLOW_URL_REQUEST'])
+                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    });
 
-        $app->add(function ($req, $res, $next) {
-            $response = $next($req, $res);
-            return $response
-                    ->withHeader('Access-Control-Allow-Origin', $_SERVER['ALLOW_URL_REQUEST'])
-                    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-                    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-        });
+    $app->get('/', 'App\Controller\DefaultController:getHelp');
+    $app->get('/status', 'App\Controller\DefaultController:getStatus');
+    $app->post('/login', \App\Controller\User\Login::class);
 
-        $app->get('', 'App\Controller\DefaultController:getHelp');
-        $app->get('/status', 'App\Controller\DefaultController:getStatus');
-        $app->post('/login', \App\Controller\User\Login::class);
-
-        $app->group('/users', function () use ($app): void {
-            $app->post('', User\Create::class);
-            $app->get('/{id}', User\GetOne::class)->add(new Auth());
-            $app->put('/{id}', User\Update::class)->add(new Auth());
-            // $app->delete('/{id}', User\Delete::class)->add(new Auth());
-        });
-        // });  
-        
-        // Catch-all route to serve a 404 Not Found page if none of the routes match
-        // NOTE: make sure this route is defined last
-        $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
-            $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
-            return $handler($req, $res);
-        });
+    $app->group('/users', function () use ($app): void {
+        $app->post('', User\Create::class);
+        $app->get('/{id}', User\GetOne::class)->add(new Auth());
+        $app->put('/{id}', User\Update::class)->add(new Auth());
+        // $app->delete('/{id}', User\Delete::class)->add(new Auth());
+    });
+    // });  
+    
+    // Catch-all route to serve a 404 Not Found page if none of the routes match
+    // NOTE: make sure this route is defined last
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+        $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+        return $handler($req, $res);
     });
         
     return $app;
