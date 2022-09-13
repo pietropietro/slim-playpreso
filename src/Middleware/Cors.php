fcf68b2474ab0815cdb5e4923620c1d81784c1f8
pietro <pietro@playpreso.com>
2022-09-13 12:55:27 +0200

    enable local debugging on public ip

--- src/Middleware/Cors.php
+++ src/Middleware/Cors.php
@@ -14,7 +14,7 @@ final class Cors extends Base
         Response $response,
     ): Response {
         return $response
-            ->withHeader('Access-Control-Allow-Origin', $_SERVER['ALLOW_URL_REQUEST'])
+            ->withHeader('Access-Control-Allow-Origin', $_ENV['ALLOW_URL_REQUEST'])
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
             ->withHeader('Access-Control-Allow-Credentials', 'true')
