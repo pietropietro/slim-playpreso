a19558a1f72aaa9eda41de3076136bea51d272fa
pietro <pietro@playpreso.com>
2023-01-02 15:51:42 +0100

    dropped p-league columns user & round count

--- src/Repository/PPLeagueRepository.php
+++ src/Repository/PPLeagueRepository.php
@@ -26,8 +26,6 @@ final class PPLeagueRepository extends BaseRepository
         $data = array(
 			"ppTournamentType_id" => $typeId,
 			"created_at" => $this->db->now(),
-			"user_count" => 0,
-            "round_count" => 0
 	    );
         return $this->db->insert('ppLeagues',$data);
     }
@@ -35,7 +33,6 @@ final class PPLeagueRepository extends BaseRepository
     function setStarted(int $id) {
         $data = array(
             "started_at" => $this->db->now(),
-            "round_count" => 1
         );
         $this->db->where('id', $id);
         $this->db->update('ppLeagues', $data, 1);
@@ -49,23 +46,9 @@ final class PPLeagueRepository extends BaseRepository
         $this->db->update('ppLeagues', $data, 1);
     }
 
-
-    function incUserCount(int $id){
-        $this->db->query("UPDATE ppLeagues SET user_count=user_count+1 WHERE id=$id");
-    }
-
-    function incRoundCount(int $id) {
-        $data = array(
-            "round_count" => $this->db->inc()
-        );
-        $this->db->where('id', $id);
-        $this->db->update('ppLeagues', $data, 1);
-    }
-
     function getJoinable(int $typeId){
         $this->db->where('ppTournamentType_id', $typeId);
         $this->db->where('started_at IS NULL');
-        $this->db->where('user_count', 20, "<");
        
         return $this->db->getOne('ppLeagues');
     }
