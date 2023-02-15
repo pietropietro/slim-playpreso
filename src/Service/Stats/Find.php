0d62cdd4545094f98ac38b55a3436836deb9f00f
pietro <pietro@playpreso.com>
2023-02-15 12:00:13 +0100

    return stat as obj

--- src/Service/Stats/Find.php
+++ src/Service/Stats/Find.php
@@ -17,20 +17,29 @@ final class Find extends BaseService{
     public function bestUsers(?int $userId = null) {
         $aggregates = $this->statsRepository->bestUsers();
 
-        
-        if($userId && !in_array($userId, array_column($aggregates,'user_id'))){
-            $user_extra_stats = $this->statsRepository->bestUsers($userId)[0];
-            array_push($aggregates, $user_extra_stats);
+        foreach ($aggregates as &$value) {
+            $this->enrich($value);
         }
 
-        foreach ($aggregates as &$value) {
-            $value['user'] = array(
-                "username" => $value['username'],
-                "id" => $value['user_id'],
-                "trophies" => $this->userParticipationFindService->getTrophies($value['user_id'])
-            );
+        $returnArray = array(
+            "best" => $aggregates,
+        );
+
+        if($userId && !in_array($userId, array_column($aggregates,'user_id'))){
+            $user_extra_stats = $this->statsRepository->bestUsers($userId)[0];
+            $this->enrich($user_extra_stats);
+            $returnArray['currentUserStat'] = $user_extra_stats;
         }
-        return $aggregates;
+
+        return $returnArray;
+    }
+
+    private function enrich(&$userStat){
+        $userStat['user'] = array(
+            "username" => $userStat['username'],
+            "id" => $userStat['user_id'],
+            "trophies" => $this->userParticipationFindService->getTrophies($userStat['user_id'])
+        );
     }
 
 }
