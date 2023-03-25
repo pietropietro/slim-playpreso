d12e6900a57377af1ffd9278c33f860093046cde
pietro <pietro@playpreso.com>
2023-03-25 08:07:08 +0000

    Revert "Revert "admin.pp.com cors try but commented""
    This reverts commit 933ef39256128ac7ad0dc0e1bd58e565bf8b1429.
    
    undo revert

--- src/Middleware/Cors.php
+++ src/Middleware/Cors.php
@@ -13,6 +13,24 @@ final class Cors extends Base
         Request $request,
         Response $response,
     ): Response {
+
+        //admin.playpreso.com try
+        //it was working but had nginx issues
+        // $allowedOrigins = explode(',', $_ENV['ALLOW_URL_REQUEST']);
+        // $origin = $request->getHeaderLine('Origin');
+    
+        // if (in_array($origin, $allowedOrigins)) {
+        //     return $response
+        //         ->withHeader('Access-Control-Allow-Origin', $origin)
+        //         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        //         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+        //         ->withHeader('Access-Control-Allow-Credentials', 'true')
+        //         ->withHeader('Access-Control-Expose-Headers', 'Authorization');
+        // } else {
+        //     return $response->withStatus(403, 'Forbidden');
+        // }
+
+
         return $response
             ->withHeader('Access-Control-Allow-Origin', $_ENV['ALLOW_URL_REQUEST'])
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
