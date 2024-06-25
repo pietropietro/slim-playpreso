3f1a69260ecf8985166ac4cc4d2dae46087819df
pietro <pietro@playpreso.com>
2024-06-26 00:16:22 +0200

    v1.45

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Slim\Http\Response;
 
 final class DefaultController extends BaseController
 {
-    private const API_VERSION = '1.44a' ;
+    private const API_VERSION = '1.45' ;
 
     public function getHelp(Request $request, Response $response): Response
     {
