b3ec26b258c0b32187cb2d4ceee0223df664a0a9
pietro <pietro@playpreso.com>
2024-10-04 17:39:22 +0200

    removed comment

--- src/Middleware/Cors.php
+++ src/Middleware/Cors.php
@@ -14,22 +14,6 @@ final class Cors extends Base
         Response $response,
     ): Response {
 
-        //admin.playpreso.com try
-        //it was working but had nginx issues
-        // $allowedOrigins = explode(',', $_ENV['ALLOW_URL_REQUEST']);
-        // $origin = $request->getHeaderLine('Origin');
-    
-        // if (in_array($origin, $allowedOrigins)) {
-        //     return $response
-        //         ->withHeader('Access-Control-Allow-Origin', $origin)
-        //         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        //         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-        //         ->withHeader('Access-Control-Allow-Credentials', 'true')
-        //         ->withHeader('Access-Control-Expose-Headers', 'Authorization');
-        // } else {
-        //     return $response->withStatus(403, 'Forbidden');
-        // }
-
 
         return $response
             ->withHeader('Access-Control-Allow-Origin', $this->getAllowedOrigin($request))
