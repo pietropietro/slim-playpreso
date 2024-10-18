2398d80718a76908ec9ea08705ec27ce3866a56e
pietro <pietro@playpreso.com>
2024-10-18 18:49:49 +0200

    avoid a push notification error to stop the guess verification

--- src/Service/Guess/Verify.php
+++ src/Service/Guess/Verify.php
@@ -21,18 +21,22 @@ final class Verify extends BaseService{
     public function verify(int $matchId, int $scoreHome, int $scoreAway){
         $guesses = $this->guessRepository->getForMatch($matchId, not_verified: false);
 
+        
         foreach ($guesses as $key => $guess) {
             $result = $this->pointsCalculateService->calculate($scoreHome,$scoreAway,$guess['home'],$guess['away']);
             $this->guessRepository->verify($guess['id'], $result['unox2'], $result['uo25'], $result['ggng'], $result['preso'], $result['points']);
             $this->pointsUpdateService->plus($guess['user_id'], $result['points']);
+        }
 
+        // 2 different foreach to avoid a push notification error to stop the guess verification
+        foreach ($guesses as $key => $guess) {
             $this->userNotificationCreateService->create(
                 $guess['user_id'],
                 'guess_verified',
                 $guess['id'], 
             );
-          
         }
+
     }
 
     public function setMissed(){
